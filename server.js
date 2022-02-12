d5b7e5537d2de3f300e88e8c470d79bd200016f9
VaishnaviMudaliar <vaishnavim1311@gmail.com>
2022-02-12 13:41:08 +0530

    set the view engine

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 
 const app = express();
+
+dotenv.config({ path: 'config.env' });
+const PORT = process.env.PORT || 8080;
+
+// log request
+app.use(morgan('tiny'));
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// set view engine
+app.set('view engine', 'ejs');
+app.set('views');
 
 app.get('/', (req, res) => {
   res.send('Crud Application');
 });
 
-app.listen(3000, () => {
-  console.log(`Server is running on http://localhost:${3000}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
