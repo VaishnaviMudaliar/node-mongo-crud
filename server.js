35d73d36b6634903ecae333236ceeb0895b686ec
VaishnaviMudaliar <vaishnavim1311@gmail.com>
2022-02-25 13:31:54 +0530

    1 file modified

--- server.js
+++ server.js
@@ -18,11 +18,19 @@ app.use(bodyparser.urlencoded({ extended: true }));
 
 // set view engine
 app.set('view engine', 'ejs');
-app.set('views');
+// app.set('views',path.resolve(__dirname));
+
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+
+
 
 app.get('/', (req, res) => {
   res.send('Crud Application');
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
