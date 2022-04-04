c5bb69b7a86ed69e3068a94f3166a81e5d4799ac
VaishnaviMudaliar <vaishnavim1311@gmail.com>
2022-04-04 12:43:39 +0530

    1 file modified

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -6,3 +6,7 @@ exports.create = (req, res) => {};
 // retrieve and return all users/retrieve and return a single user
 
 exports.find = (req, res) => {};
+
+// update a new identified user by user id
+
+exports.update = (req,res) =>{}
