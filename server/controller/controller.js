6959c750945a41f5b9baa0e1b05fb72b5c02e5ff
VaishnaviMudaliar <vaishnavim1311@gmail.com>
2022-04-04 12:59:46 +0530

    2 files modified

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -9,4 +9,8 @@ exports.find = (req, res) => {};
 
 // update a new identified user by user id
 
-exports.update = (req,res) =>{}
+exports.update = (req, res) => {};
+
+// delete a user with specified user id in the request
+
+exports.delete = (req, res) => {};
