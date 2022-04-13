69fc26f453979a1ec8efc82e84a11aafdf745a17
VaishnaviMudaliar <vaishnavim1311@gmail.com>
2022-04-13 12:10:52 +0530

    1 file modified

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -7,6 +7,26 @@ exports.create = (req, res) => {
     res.status(400).send({ message: 'Content cannot be empty' });
     return;
   }
+  // new user
+  const user = new Userdb({
+    name :req.body.name,
+    email: req.body.email,
+    gender : req.body.gender,
+    status: req.body.status
+  })
+
+
+  // save user in the database
+  user
+    .save(user)
+    .then(data =>{
+      res.send(data)
+    })
+    .catch(err => {
+      res.status(500).send({
+        message : err.message || "Some error occure"
+      })
+    })
 };
 
 // retrieve and return all users/retrieve and return a single user
