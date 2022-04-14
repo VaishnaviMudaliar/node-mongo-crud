9f40d7ec899eb686e6e56224744c3149ca9af638
VaishnaviMudaliar <vaishnavim1311@gmail.com>
2022-04-14 18:50:29 +0530

    1 file modified, 1 deleted

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -9,50 +9,63 @@ exports.create = (req, res) => {
   }
   // new user
   const user = new Userdb({
-    name :req.body.name,
+    name: req.body.name,
     email: req.body.email,
-    gender : req.body.gender,
-    status: req.body.status
-  })
-
+    gender: req.body.gender,
+    status: req.body.status,
+  });
 
   // save user in the database
   user
     .save(user)
-    .then(data =>{
-      res.send(data)
+    .then((data) => {
+      res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message : err.message || "Some error occured"
-      })
-    })
+        message: err.message || 'Some error occured',
+      });
+    });
 };
 
 // retrieve and return all users/retrieve and return a single user
 
 exports.find = (req, res) => {
   Userdb.find()
-  .then(user => {
-    res.send(user)
-  })
+    .then((user) => {
+      res.send(user);
+    })
 
-.catch(err => {
-  res.status(500).send({
-    message : err.message||'Some error occured'
-  })
-})
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Some error occured',
+      });
+    });
 };
 
 // update a new identified user by user id
 
 exports.update = (req, res) => {
-  if(!req.body){
-    return res.status(400).send({message : "data to be updated cannot be empty"})
+  if (!req.body) {
+    return res
+      .status(400)
+      .send({ message: 'data to be updated cannot be empty' });
   }
 
   const id = req.params.id;
-  
+  Userdb.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update user with ${id}. Maybe user not found`,
+        });
+      } else {
+        res.send(data);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: 'Error!!!!!!' });
+    });
 };
 
 // delete a user with specified user id in the request
