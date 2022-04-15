9b1fceaa48e34ee0cb7f42544d56c3a869605fbe
VaishnaviMudaliar <vaishnavim1311@gmail.com>
2022-04-15 16:51:14 +0530

    3 files modified

--- server/services/render.js
+++ server/services/render.js
@@ -17,5 +17,13 @@ exports.add_user = (req, res) => {
 };
 
 exports.update_user = (req, res) => {
+  axios
+    .get('http://localhost:3000/api/users', { params: { id: req.query.id } })
+    .then(function (userdata) {
+      res.render('_update_user', { user: userdata.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
   res.render('_update_user');
 };
