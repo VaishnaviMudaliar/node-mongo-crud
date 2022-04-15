4de04a2060fa1fe0c9e6927d8eb9c2d1bcca10f7
VaishnaviMudaliar <vaishnavim1311@gmail.com>
2022-04-15 17:11:27 +0530

    2 files modified

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,25 @@
 $('#add_user').submit(function (event) {
   alert('Data inserted successfully');
 });
+
+$('#update_user').submit(function (event) {
+  event.preventDefault();
+
+  var unindexed_array = $('#update_user').serializeArray();
+  var data = {};
+  $.map(unindexed_array, function (n, i) {
+    data[n['name']] = n['value'];
+  });
+  console.log(data);
+
+  var request = {
+    'url' : `http://localhost:3000/api/users/${data.id}`,
+    'method' : "PUT",
+    'data' : data
+  }
+
+
+  $.ajax(request).done(function(response){
+    alert('Data Updated Succes')
+  })
+});
