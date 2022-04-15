d91812936c95e373f665a4313b72cb20cef7315d
VaishnaviMudaliar <vaishnavim1311@gmail.com>
2022-04-15 17:21:34 +0530

    2 files modified

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,6 +20,28 @@ $('#update_user').submit(function (event) {
 
 
   $.ajax(request).done(function(response){
-    alert('Data Updated Succes')
+    alert('Data Updated successfully')
   })
 });
+
+
+if(window.location.pathname == "/"){
+  $ondelete = $(".table tbody td a.delete");
+  $ondelete.click(function(){
+    var id = $(this).attr("data-id")
+  })
+
+
+  var request = {
+    'url' : `http://localhost:3000/api/users/${id}`,
+    'method' : "DELETE",
+   
+  }
+  if(confirm("Do you really want to delete this record?")){
+    $.ajax(request).done(function(response){
+      alert('Data Deleted successfully')
+      location.reload()
+    })
+  }
+
+}
